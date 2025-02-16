276c8c2ead0d7ed7f3198301e8f3561ddd9d6902
Ahmed Nasreldin Elnour <118545981+AhmedNasreldin4869@users.noreply.github.com>
2025-02-16 19:57:09 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.jpg" />
+        <title> GalmourWalk </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+    
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+     
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+      
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>We sell luxurious shoes .</h1>
+                       <p>Flat 40% OFF on our premium brands.</p>
+                       <a href="products.php" class="btn btn-danger">Shop Now</a>
+                   </div>
+                   </center>
+               </div>
+           </div>
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-4">
+                       <div  class="thumbnail">
+                           <a href="products.php">
+                                <img src="img/heels 1.jpg" alt="Camera">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">Heels</p>
+                                        <p>Choose among the best available in the world.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/boots 1.jpg" alt="Watch">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                    <p id="autoResize">BOOTS</p>
+                                    <p>Original watches from the best brands.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/flat 1.jpg" alt="Shirt">
+                           </a>
+                           <center>
+                               <div class="caption">
+                                   <p id="autoResize">FlatShoes</p>
+                                   <p>Our exquisite collection of Flat Shoes.</p>
+                               </div>
+                           </center>
+                       </div>
+                   </div>
+               </div>
+           </div>
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy <a href="">GalmourWalk</a> Store. All Rights Reserved.</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
